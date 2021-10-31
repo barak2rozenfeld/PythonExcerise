d2243074af8ed26cd57a548edb15994479c4e945
barak2rozenfeld <barak2rozenfeld@gmail.com>
2021-10-31 15:36:38 +0200

    Add files via upload

---
+++ dateTimeValid.py
@@ -0,0 +1,17 @@
+y = int(input("Year: \n"))
+m = int(input("Month: \n"))
+d = int(input("Day: \n"))
+
+
+day_count_for_month = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+if y%4==0 and (y%100 != 0 or y%400==0):
+    day_count_for_month[2] = 29
+
+if (y>0 and m<13):
+    if (d<=day_count_for_month[m]):
+        print ("This Date is valid")
+    else:
+        print ("This date is not valid")
+else:
+    print ("this date is not valud.")
+
